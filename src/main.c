53ead0e2ba08740ec29e606df553e7c42fd4e8f0
Mahdi Hosseini Asaad <mahdi.libremann@gmail.com>
2026-02-12 22:36:51 +0330

    Update: main.c | Add: File Support

--- src/main.c
+++ src/main.c
@@ -7,19 +7,33 @@
 #include "colors.h"
 // #include "pstring.h"
 
-int main(void) {
-    unsigned char *code = NULL;
-    unsigned int CodeSize=0;
-    char input[256];
-    unsigned int HEX_INT=0;
+#define PROMPT ">>"
 
-    show_startup();
+int main(int argc, char **argv) {
+    unsigned char *code = NULL; /* opcodes in Memory */
+    unsigned int CodeSize=0; /* opcodes size */
+    char input[256]; /* input buffer*/
+    unsigned int HEX_INT=0; /* input buffer to Hex buffer */
+    FILE *inp_ptr;
+
+    if(argc==1) { /* console */
+        show_startup();
+        inp_ptr=stdin;
+    } else { /* file */
+        inp_ptr=fopen(argv[1], "r");
+        if(!inp_ptr) {
+            printf(YELLOW"FileError:"RESET"Unable to open '%s'.", argv[1]);
+            return 1;
+        }
+    }
 
     while(1) {
-        printf(PINK">> "RESET);
-        if(fgets(input, sizeof(input), stdin) == NULL) {printf(YELLOW"InputError:"RESET" An error occurred in the input buffer.\n");}
-        if(strcmp(input, "\n")==0) continue;
-        input[strcspn(input, "\n")]=0;
+        if(argc==1) printf(PINK"%s "RESET, PROMPT);
+
+        if(fgets(input, sizeof(input), inp_ptr) == NULL) {printf(YELLOW"InputError:"RESET" An error occurred in the input buffer.\n");}
+        
+        if(strcmp(input, "\n")==0) continue; /* ignore blank line */
+        input[strcspn(input, "\n")]=0; /* Remove NUL */
         
         
         if(input[0] == ';') continue;
@@ -28,12 +42,11 @@ int main(void) {
         if(strcmp(input, "HLP")==0) {load_hlp(); continue;}
         if(strcmp(input, "CPY")==0) {load_cpy(); continue;}
         if(strcmp(input, "CLR")==0) {load_clr(); continue;}
-    
-    
-        if(sscanf(input, "%X", &HEX_INT)) {
+
+        if(sscanf(input, "%X", &HEX_INT) == 1) {
             code = (unsigned char *) realloc (code, sizeof(char)*(CodeSize+1));
             if(!code){perror("realloc"); return 1;}
-            code[CodeSize]=HEX_INT;
+            code[CodeSize]= (unsigned char) HEX_INT;
             ++CodeSize;
         } else {
             printf(YELLOW"InputError:"RESET" You are only allowed to enter hexadecimal numbers or 'RUN' and 'END'.\n");
@@ -42,7 +55,7 @@ int main(void) {
 
     if(!CodeSize) {perror(YELLOW"EmptyBuffer"RED); return 1;}
 
-    void *mem = mmap(NULL, sizeof(code), PROT_READ | PROT_WRITE | PROT_EXEC,
+    void *mem = mmap(NULL, CodeSize, PROT_READ | PROT_WRITE | PROT_EXEC,
                      MAP_ANON | MAP_PRIVATE, -1, 0);
     if (mem == MAP_FAILED) { perror("mmap"); return 1; }
 
@@ -51,9 +64,10 @@ int main(void) {
     void (*fn)(void) = mem;
     fn();
 
-    munmap(mem, sizeof(code));
+    munmap(mem, CodeSize);
 
 Exit:
+    if(inp_ptr!=stdin) fclose(inp_ptr);
     free(code);
     return 0;
 }

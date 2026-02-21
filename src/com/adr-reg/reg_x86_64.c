daaeef9175069c5bb6af106e9f407d2f63aa67b9
Mahdi Hosseini Asaad <mahdi.libremann@gmail.com>
2026-02-21 14:48:58 +0330

    Update: reg-initialization.mi adr-reg/reg_x86_64.c
     The function structure has been improved, and the changes have been applied to the corresponding example.

--- src/com/adr-reg/reg_x86_64.c
+++ src/com/adr-reg/reg_x86_64.c
@@ -1,33 +1,42 @@
 #include <stdio.h> /* printf */
 
+#define MOVREG(register) __asm__("mov %"#register", %0" : "=r" (register))
+
 void reg_x86_64() {
-    unsigned long long RAX, RBX, RCX, RDX, RSI, RDI, RSP, RBP, R8, R9, R10, R11, R12, R13, R14, R15;
-    __asm__ ("mov %%rax, %0" : "=r" (RAX));
-    __asm__ ("mov %%rbx, %0" : "=r" (RBX));
-    __asm__ ("mov %%rcx, %0" : "=r" (RCX));
-    __asm__ ("mov %%rdx, %0" : "=r" (RDX));
-    __asm__ ("mov %%rsi, %0" : "=r" (RSI));
-    __asm__ ("mov %%rdi, %0" : "=r" (RDI));
-    __asm__ ("mov %%rsp, %0" : "=r" (RSP));
-    __asm__ ("mov %%rbp, %0" : "=r" (RBP));
-    __asm__ ("mov %%r8,  %0" : "=r" (R8));
-    __asm__ ("mov %%r9,  %0" : "=r" (R9));
-    __asm__ ("mov %%r10, %0" : "=r"(R10));
-    __asm__ ("mov %%r11, %0" : "=r"(R11));
-    __asm__ ("mov %%r12, %0" : "=r"(R12));
-    __asm__ ("mov %%r13, %0" : "=r"(R13));
-    __asm__ ("mov %%r14, %0" : "=r"(R14));
-    __asm__ ("mov %%r15, %0" : "=r"(R15));
+    unsigned long long registers[16];
+    const char *regs_x86_64[16] = {
+        "rax", "rbx", "rcx", "rdx",
+        "rsi", "rdi", "rsp", "rbp",
+        "r8" , "r9" , "r10", "r11",
+        "r12", "r13", "r14", "r15"
+    };
 
-    printf("   RAX=%llx\n   RBX=%llx\n", RAX, RBX);
-    printf("   RCX=%llx\n   RDX=%llx\n", RCX, RDX);
-    printf("   RSI=%llx\n   RDI=%llx\n", RSI, RDI);
-    printf("   R8=%llx \n   R9=%llx \n",R8 , R9 );
-    printf("   R10=%llx\n   R11=%llx\n",R10, R11);
-    printf("   R12=%llx\n   R13=%llx\n",R12, R13);
-    printf("   R14=%llx\n   R15=%llx\n",R14, R15);
+    __asm__ ("mov %%rax, %0" : "=r" (registers[0]));
+    __asm__ ("mov %%rbx, %0" : "=r" (registers[1]));
+    __asm__ ("mov %%rcx, %0" : "=r" (registers[2]));
+    __asm__ ("mov %%rdx, %0" : "=r" (registers[3]));
+    __asm__ ("mov %%rsi, %0" : "=r" (registers[4]));
+    __asm__ ("mov %%rdi, %0" : "=r" (registers[5]));
+    __asm__ ("mov %%rsp, %0" : "=r" (registers[6]));
+    __asm__ ("mov %%rbp, %0" : "=r" (registers[7]));
+    __asm__ ("mov %%r8,  %0" : "=r" (registers[8]));
+    __asm__ ("mov %%r9,  %0" : "=r" (registers[9]));
+    __asm__ ("mov %%r10, %0" : "=r" (registers[10]));
+    __asm__ ("mov %%r11, %0" : "=r" (registers[11]));
+    __asm__ ("mov %%r12, %0" : "=r" (registers[12]));
+    __asm__ ("mov %%r13, %0" : "=r" (registers[13]));
+    __asm__ ("mov %%r14, %0" : "=r" (registers[14]));
+    __asm__ ("mov %%r15, %0" : "=r" (registers[15]));
 
-    printf("   RSP=%llx\n",RSP);
-    printf("   RBP=%llx\n", RBP);
+    printf("   RAX=%llx\n   RBX=%llx\n", registers[0] , registers[1]);
+    printf("   RCX=%llx\n   RDX=%llx\n", registers[2] , registers[3]);
+    printf("   RSI=%llx\n   RDI=%llx\n", registers[4] , registers[5]);
+    printf("   R8=%llx \n   R9=%llx \n", registers[6] , registers[7] );
+    printf("   R10=%llx\n   R11=%llx\n", registers[8] , registers[9]);
+    printf("   R12=%llx\n   R13=%llx\n", registers[10], registers[11]);
+    printf("   R14=%llx\n   R15=%llx\n", registers[12], registers[13]);
+
+    printf("   RSP=%llx\n", registers[14]);
+    printf("   RBP=%llx\n", registers[15]);
     return;
 }

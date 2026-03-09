c8d5ae8c76016a320d410010fc52614637cbcf02
Mahdi Hosseini Asaad <mahdi.libremann@Gmail.com>
2026-03-09 20:20:55 +0330

    Update: ldf.c | Fix: printf File opcodes problem

--- src/com/ldf.c
+++ src/com/ldf.c
@@ -73,11 +73,14 @@ int zelemi_command_lod(int argc, char *opt, struct DATA_STRUCT *data_pack) {
             input[strcspn(input, "\0")]=' '; /* Remove NUL */
             printf("; %s\n", input); // flag=1; 
             continue; 
-        } else printf(GREEN"%s\n"RESET, input);
+        }
 
         input[strcspn(input, "\0")]=' '; /* Remove NUL */
+        printf(GREEN"%s\n"RESET, input);
+
         if(zelemi_send(input, data_pack)) goto RET_3;
     }
+
     // if(flag) zelemi_printerr_sys("Hint", COMMAND_LOAD_HINT);
 
 RET_3:
